dded6362c8a5fa5b06fe23089f5f2077d58419f4
Kloudie <cloudiekass@gmail.com>
2025-12-20 11:11:03 +0800

    consultations问诊会话姓名显示

--- consultations/serializers.py
+++ consultations/serializers.py
@@ -17,7 +17,9 @@ class MessageSerializer(serializers.ModelSerializer):
 class ConsultationSerializer(serializers.ModelSerializer):
     """问诊会话序列化器"""
     user_id = serializers.IntegerField(source='user.id', read_only=True)
+    user_name = serializers.CharField(source='user.name', read_only=True)
     doctor_id = serializers.IntegerField(source='doctor.id', read_only=True)
+    doctor_name = serializers.CharField(source='doctor.name', read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
     
     class Meta:
